2db818326c5e6a3aa0e6d927e995a34597405cf5
Martin Wolf <martin@martinwolf.org>
2015-01-11 14:15:21 +0100

    Load fonts async

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 
     <title>gif.martinwolf.org</title>
 
+    <script>
+        WebFontConfig = {
+            google: { families: [ 'Lato:400,900:latin' ] }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })();
+    </script>
+
     <link rel="stylesheet" href="dist/css/style.min.css">
 </head>
 
